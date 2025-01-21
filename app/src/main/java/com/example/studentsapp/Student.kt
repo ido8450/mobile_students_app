a21dcdf82832f7c399cd3a5e96a3d006a6ba5107
ido8450 <ido8450@gmail.com>
2025-01-21 16:57:00 +0200

    added base student data singelton

---
+++ app/src/main/java/com/example/studentsapp/Student.kt
@@ -0,0 +1,7 @@
+package com.example.studentsapp
+
+data class Student(
+    var id: String,
+    var name: String,
+    var isChecked: Boolean = false
+)
---
+++ app/src/main/java/com/example/studentsapp/StudentRepository.kt
@@ -0,0 +1,5 @@
+package com.example.studentsapp
+
+object StudentRepository {
+    val students = mutableListOf<Student>()
+}
